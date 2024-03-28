fd1c636ff78ac05375a9d08b62c60dfabe60c58b
Martin Kribs <martin.kribs.mk@gmail.com>
2024-03-28 10:46:04 +0100

    update vorlesung 3

---
+++ uebung_1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# 1
+file = pd.read_csv("./DataSet/Auto_ml.csv", sep=";")
+print(file.head(10))
+
+# 2
+print()
+print(file.isnull().sum())
+
+# 3
+print()
+
+# 4
+print()
+
+# 5
+print()
+
+# 6
+print()
--- vorlesung_3.py
+++ vorlesung_3.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Einlesen der Datei
 file = pd.read_csv("./DataSet/data_health.csv", sep=";", decimal=".")
@@ -9,5 +11,46 @@ print(file.head(10))
 print("\n Anzahl Leerzeilen:\n", file.isnull().sum())
 
 # Verfahren zum Füllen der Leerzeilen
-file.dropna(axis=0,how="all",inplace=True)
-print(file.head)
+file.dropna(axis=0, how="all")
+print(file.head(6))
+
+file["Duration"] = file["Duration"].fillna(45)
+print(file[20:25])
+
+file["Pulse"] = file["Pulse"].ffill()
+
+mw = file["Maxpulse"].median()
+print("Median: ", mw)
+file["Maxpulse"] = file["Maxpulse"].fillna(mw)
+
+mw = file["Calories"].mean().round(1)
+print("Mittelwert 2: ", mw)
+file["Calories"] = file["Calories"].fillna(mw)
+
+# Probeausgabe
+print("Probe Leerzeilen: ")
+print(file.isnull().sum(), "\n")
+
+# univariate
+print(file.median())
+
+# einfache Methode
+print(file.describe().round(1))
+
+# Pairplot
+# sns.pairplot(file)
+# plt.show()
+
+# Boxplot
+# sns.boxplot(file, showfliers=True)
+# plt.xlabel(["Duration"],["Puls"],["Maxpulse"],["Calories"])
+# plt.show()
+
+# Berechnung der
+print()
+print(file.corr(method="spearman").round(2))
+
+# Heatmap
+plt.title("Heatmap von Data Health")
+sns.heatmap(file.corr(),annot=True)
+plt.show()
